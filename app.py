637c5a95780f3111e9517fc8256e0b6a416e49b7
prathmesh2102003 <prathmeshamale646@gmail.com>
2025-02-21 15:52:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello from github actions")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, GitHub Actions! Your Flask app is running successfully."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
